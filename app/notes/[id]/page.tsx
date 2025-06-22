b2e13f30460eedaaf3c7676c2ed728bf33c37584
Gleb Voronov <glebvoronov309@gmail.com>
2025-06-22 14:57:21 +0300

    fixed

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,13 +1,12 @@
-import css from './NoteDetails.module.css';
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
 import { fetchNoteById } from '../../../lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
-interface NoteDetailsPageProps {
+export default async function NoteDetailsPage({
+  params,
+}: {
   params: { id: string };
-}
-
-export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
+}) {
   const id = Number(params.id);
   const queryClient = new QueryClient();
 
@@ -17,10 +16,8 @@ export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) 
   });
 
   return (
-    <main className={css.container}>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <NoteDetailsClient id={id} />
-      </HydrationBoundary>
-    </main>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient id={id} />
+    </HydrationBoundary>
   );
 }
