886dea15135e7741684e6a0f630663b5495fcf42
Gleb Voronov <glebvoronov309@gmail.com>
2025-06-22 15:17:21 +0300

    fixed the error

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,11 +3,7 @@ import { fetchNoteById } from '../../../lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 import { notFound } from 'next/navigation';
 
-type PageProps = {
-  params: { id: string };
-};
-
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
   const queryClient = new QueryClient();
 
